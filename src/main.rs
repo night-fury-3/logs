3fefa903859c88938f640832dfa57e8bebe483df
night-fury-3 <jonathonfrakes3@gmail.com>
2024-08-20 16:10:52 -0400

    7. Handling the unexpected Errors and Results
    feat: The Result Enum in Action

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::fs;
-
+use std::io::Error;
 fn main() {
     let text = fs::read_to_string("logs.txt");
 
     println!("{:#?}", text);
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        Err(Error::other("Can't divide by 0"))
+    } else {
+        Ok(a / b)
+    }
 }
