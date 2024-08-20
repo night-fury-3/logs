1537cc44f7e3833087c9c3bd2b0da932606a996f
night-fury-3 <jonathonfrakes3@gmail.com>
2024-08-20 16:18:42 -0400

    7. Handling the unexpected Errors and Results
    feat: Matching on Results

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::fs;
 use std::io::Error;
 fn main() {
-    let text = fs::read_to_string("logs.txt");
+    // let text = fs::read_to_string("logs.txt");
 
-    println!("{:#?}", text);
+    // println!("{:#?}", text);
+
+    match divide(5.0, 0.0) {
+        Ok(result_of_division) => {
+            println!("{}", result_of_division);
+        }
+        Err(what_wnt_wrong) => {
+            println!("{}", what_wnt_wrong);
+        }
+    }
 }
 
 fn divide(a: f64, b: f64) -> Result<f64, Error> {
