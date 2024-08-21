4c03b9efd00a879be1359fcc9836553237c8505c
night-fury-3 <jonathonfrakes3@gmail.com>
2024-08-21 15:09:44 -0400

    7. Handling the unexpected Errors and Results
    feat: Finding Error Logs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
+use core::error;
 use std::fs;
 
-fn string_test(a: String, b: &String, c: &str) {}
+fn extract_errors(text: &str) -> Vec<&str> {
+    let split_text = text.split("\n");
+
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line);
+        }
+    }
+
+    results
+}
 
 fn main() {
-    string_test(
-        "red".to_string(),
-        &String::from("red"),
-        String::from("red").as_str(),
-    );
+    let mut error_logs = vec![];
 
-    match fs::read_to_string("losgs.txt") {
+    match fs::read_to_string("logs.txt") {
         Ok(text_that_was_read) => {
-            println!("{}", text_that_was_read);
+            error_logs = extract_errors(text_that_was_read.as_str());
+            println!("{:#?}", error_logs);
         }
         Err(why_this_failed) => {
             println!("Failed to read file: {}", why_this_failed)
         }
     }
+
+    println!("{:#?}", error_logs)
 }
